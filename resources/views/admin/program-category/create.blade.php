ae5a5573f002369c3fbc7251398853b5add580a1
hawkxc <hawkxc068@gmail.com>
2025-06-14 16:08:15 +0700

    add : add programCategory views

---
+++ resources/views/admin/program-category/create.blade.php
@@ -0,0 +1,219 @@
+@extends('layouts.admin', [
+    'second_title' => 'Kategori Program',
+    'header_title' => 'Tambah Kategori',
+    'sidebar_menu' => 'program',
+    'sidebar_submenu' => 'category',
+])
+
+@section('css_plugins')
+@endsection
+
+
+@section('css_inline')
+    <style type="text/css">
+        .required:after {
+            content: "*";
+            color: red;
+        }
+    </style>
+@endsection
+
+
+@section('content')
+    <div class="main-card mb-3 card">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-5">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb mb-0 pb-0 pl-0">
+                            <li class="breadcrumb-item"><a href="{{ route('adm.index') }}">Home</a></li>
+                            <li class="breadcrumb-item active" aria-current="page"><a
+                                    href="{{ route('adm.program-category.index') }}">Kategori</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Tambah Kategori</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+            <div class="divider"></div>
+
+            @if (session('success'))
+                <div class="alert alert-success" id="success-alert">
+                    {{ session('success') }}
+                </div>
+            @elseif (session('error'))
+                <div class="alert alert-danger" id="error-alert">
+                    {{ session('error') }}
+                </div>
+            @endif
+
+            <form action="{{ route('adm.program-category.store') }}" method="post" enctype="multipart/form-data"
+                accept-charset="utf-8" class="row gy-3">
+                @csrf
+                <div class="col-12">
+                    <label class="form-label fw-semibold">Nama Kategori (max 50 karakter) {!! printRequired() !!} - <span
+                            id="count_title" class="fw-normal"></span></label>
+                    <input type="text" class="form-control form-control-sm" name="title" id="program_title"
+                        placeholder="Masukkan Nama Kategori" value="{{ old('title') }}" required>
+                    @error('title')
+                        <div class="text-danger small mt-1"><i class="ri-error-warning-line"></i> {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+                <div class="col-12">
+                    <label class="form-label fw-semibold">URL Kategori (<span class="" id="status_url">Belum
+                            Dicek</span>) {!! printRequired() !!}</label>
+                    <div class="input-group input-group-sm">
+                        <span class="input-group-text"><input type="checkbox" class="mr-2" id="edit_url"> Edit</span>
+                        <span class="input-group-text">{{ url('/') . '/programs?kategori=' }}</span>
+                        <input type="text" class="form-control" name="url" placeholder="dermawan" id="url"
+                            value="{{ old('url') }}" readonly>
+                        <span class="input-group-text p-0"><button class="btn btn-sm btn-info" id="cek_url"
+                                type="button">Cek & Lanjut</button></span>
+                    </div>
+                </div>
+
+                <div class="col-12 p-3">
+                    <div class="d-flex flex-row border p-3 rounded">
+                        <div id="inputFileContainer" class="col-12">
+                            <label class="form-label fw-semibold">Logo Kategori {!! printRequired() !!}</label>
+                            <input type="file" class="form-control form-control-sm" name="logo_image" id="imageUpload">
+                            @error('logo_image')
+                                <div class="text-danger small mt-1"><i class="ri-error-warning-line"></i> {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="mt-3 p-3 rounded col-12" style="background-color: rgb(224, 243, 255);">
+                        <div class="form-check d-flex align-items-center">
+                            <input class="form-check-input input-lg mb-2" type="checkbox" name="is_show" id="is_show">
+                            <label class="form-check-label fw-bold" for="is_show">
+                                Tampilkan Kategori
+                            </label>
+                        </div>
+                        <small class="text-dark">
+                            Kategori ini akan ditampilkan di halaman utama bantubersama.com.
+                        </small>
+                    </div>
+
+                    <div class="col-12">
+                        <div class="divider mb-2 mt-2"></div>
+                    </div>
+                    <div class="col-12 text-end">
+                        <input type="reset" class="btn btn-danger" value="Reset">
+                        <input type="submit" class="btn btn-info" value="Submit">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
+
+@section('js_plugins')
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+@endsection
+
+@section('js_inline')
+    <script type="text/javascript">
+        $(document).ready(function() {
+            setTimeout(function() {
+                $('#success-alert').fadeOut('slow');
+                $('#error-alert').fadeOut('slow');
+            }, 5000);
+
+            $('#imageUpload').on('change', function(e) {
+                const file = e.target.files[0];
+
+                if (file && file.type.startsWith('image/')) {
+                    const reader = new FileReader();
+
+                    reader.onload = function(e) {
+                        // Cek apakah imageContainer sudah ada
+                        if ($('#imageContainer').length === 0) {
+                            // Buat dan sisipkan imageContainer sebelum inputFileContainer
+                            const imageContainer = `
+                        <div id="imageContainer" class="col-2 d-flex justify-content-center align-items-center">
+                            <img src="" alt="" class="img-fluid" />
+                        </div>
+                    `;
+                            $('#inputFileContainer').removeClass('col-12').addClass(
+                                'col-10');
+                            $('#inputFileContainer').before(imageContainer);
+                        }
+
+                        // Set gambar yang dipilih ke <img>
+                        $('#imageContainer img').attr('src', e.target.result);
+                    };
+
+                    reader.readAsDataURL(file);
+                } else {
+                    alert('Silakan pilih file gambar yang valid.');
+                }
+            });
+        });
+
+        $("#program_title").on("keyup change", function() {
+            var title = $(this).val();
+            var title = title.length;
+            if (title > 50) {
+                $("#count_title").html(title + ' / 50');
+                $("#count_title").addClass('text-danger');
+            } else {
+                $("#count_title").html(title + ' / 50');
+                $("#count_title").removeClass('text-warning');
+            }
+        });
+
+        $("#program_title").on("blur", function() {
+            var title = $(this).val();
+            var title = title.toLowerCase();
+            var title = title.replace(/[^a-zA-Z0-9 ]/g, '');
+            var title = title.replace(/ /g, "-");
+            var title = title.replace(/--/g, "-");
+            var title = encodeURI(title);
+            var title = title.replace(/[^a-zA-Z0-9-]/g, '');
+            $("#url").val(title);
+        });
+
+        $("#cek_url").on("click", function() {
+            var url_data = $('#url').val();
+
+            $.ajax({
+                url: "{{ route('adm.program-category.create.check_url') }}",
+                type: "POST",
+                headers: {
+                    'X-CSRF-TOKEN': '{{ csrf_token() }}'
+                },
+                data: {
+                    "url": url_data
+                },
+                success: function(data) {
+                    if (data == 'valid') {
+                        console.log(data);
+                        $('#status_url').html('Valid');
+                        $('#status_url').removeClass('text-danger');
+                        $('#status_url').addClass('text-success');
+                    } else {
+                        console.log(data);
+                        $('#status_url').html('Sudah Dipakai');
+                        $('#status_url').removeClass('text-success');
+                        $('#status_url').addClass('text-danger');
+                    }
+
+                },
+                error: function(data) {
+                    console.log("error");
+                    console.log(data);
+                }
+            });
+        });
+
+        $("#edit_url").on("click", function() {
+            if ($("#edit_url").is(':checked')) {
+                document.getElementById('url').removeAttribute('readonly');
+            } else {
+                document.getElementById('url').readOnly = true;
+            }
+        });
+    </script>
+@endsection
