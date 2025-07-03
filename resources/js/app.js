44ecea6ea10fb678151a571e596e3f5d2b7cd872
hawkxc <hawkxc068@gmail.com>
2025-07-03 11:57:01 +0700

    update : add class import

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
 import './bootstrap';
+import 'gridstack/dist/gridstack.min.css';
+import 'gridstack/dist/gridstack-extra.min.css';
+import $ from 'jquery';
+import 'gridstack/dist/gridstack';
+import 'gridstack/dist/gridstack.jQueryUI';
