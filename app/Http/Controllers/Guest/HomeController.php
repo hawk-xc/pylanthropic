31bf9ea102d57f953c2af7b678ae924f0adc1055
hawkxc068 <hawkxc068@gmail.com>
2025-09-18 12:16:56 +0700

    update: store program and banner data on cache

--- app/Http/Controllers/Guest/HomeController.php
+++ app/Http/Controllers/Guest/HomeController.php
@@ -8,6 +8,7 @@ use App\Models;
 use App\Models\ShortLinkModel;
 use App\Models\DonaturShortLink;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
 
 class HomeController extends Controller
 {
@@ -16,62 +17,72 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $today = Carbon::today();
+        $banners = Cache::remember('banner_public', 60 * 60, function () {
+            $today = Carbon::today();
 
-        $slider = Models\Banner::where('is_publish', 1)
-            ->where('type', 'banner')
-            ->where(function ($q) use ($today) {
-                $q->where('is_forever', 1)
-                ->orWhere(function ($q2) use ($today) {
-                    $q2->whereNotNull('expire_date')
-                        ->where('expire_date', '>=', $today);
-                });
-            })
-            ->orderBy('title', 'ASC')
-            ->get();
+            $slider = Models\Banner::where('is_publish', 1)
+                ->where('type', 'banner')
+                ->where(function ($q) use ($today) {
+                    $q->where('is_forever', 1)
+                        ->orWhere(function ($q2) use ($today) {
+                            $q2->whereNotNull('expire_date')
+                                ->where('expire_date', '>=', $today);
+                        });
+                })
+                ->orderBy('title', 'ASC')
+                ->get();
 
-        $popup = Models\Banner::where('is_publish', 1)
-            ->where('type', 'popup')
-            ->where(function ($q) use ($today) {
-                $q->where('is_forever', 1)
-                ->orWhere(function ($q2) use ($today) {
-                    $q2->whereNotNull('expire_date')
-                        ->where('expire_date', '>=', $today);
-                });
-            })
-            ->first();
+            $popup = Models\Banner::where('is_publish', 1)
+                ->where('type', 'popup')
+                ->where(function ($q) use ($today) {
+                    $q->where('is_forever', 1)
+                        ->orWhere(function ($q2) use ($today) {
+                            $q2->whereNotNull('expire_date')
+                                ->where('expire_date', '>=', $today);
+                        });
+                })
+                ->first();
 
-        $category = Models\ProgramCategory::where('is_show', 1)->orderBy('sort_number', 'ASC')->get();
-        $selected = Models\Program::where('is_publish', 1)->select('program.*', 'organization.name', 'organization.status')->join('organization', 'program.organization_id', 'organization.id')->where('is_recommended', 1)->whereNotNull('program.approved_at')->where('end_date', '>', date('Y-m-d'))->orderBy('program.created_at', 'DESC')->limit(6)->get();
-        $selected->map(function ($selected, $key) {
-            $sum_amount = Models\Transaction::where('program_id', $selected->id)->where('status', 'success')->sum('nominal_final');
-            if ($selected->show_minus > 0 && !is_null($selected->show_minus) && $sum_amount > 0) {
-                return $selected->sum_amount = $sum_amount - ($sum_amount * $selected->show_minus) / 100;
-            } else {
-                return $selected->sum_amount = $sum_amount;
-            }
+            return compact('slider', 'popup');
         });
 
-        $urgent = Models\Program::select('program.*', 'organization.name', 'organization.status')->join('organization', 'program.organization_id', 'organization.id')->where('is_publish', 1)->where('is_urgent', 1)->whereNotNull('program.approved_at')->where('end_date', '>', date('Y-m-d'))->orderBy('program.created_at', 'DESC')->limit(6)->get();
-        $urgent->map(function ($urgent, $key) {
-            $sum_amount = Models\Transaction::where('program_id', $urgent->id)->where('status', 'success')->sum('nominal_final');
-            if ($urgent->show_minus > 0 && !is_null($urgent->show_minus) && $sum_amount > 0) {
-                return $urgent->sum_amount = $sum_amount - ($sum_amount * $urgent->show_minus) / 100;
-            } else {
-                return $urgent->sum_amount = $sum_amount;
-            }
+        $programs = Cache::remember('program_public', 60 * 60, function () {
+            $category = Models\ProgramCategory::where('is_show', 1)->orderBy('sort_number', 'ASC')->get();
+            $selected = Models\Program::where('is_publish', 1)->select('program.*', 'organization.name', 'organization.status')->join('organization', 'program.organization_id', 'organization.id')->where('is_recommended', 1)->whereNotNull('program.approved_at')->where('end_date', '>', date('Y-m-d'))->orderBy('program.created_at', 'DESC')->limit(6)->get();
+            $selected->map(function ($selected, $key) {
+                $sum_amount = Models\Transaction::where('program_id', $selected->id)->where('status', 'success')->sum('nominal_final');
+                if ($selected->show_minus > 0 && !is_null($selected->show_minus) && $sum_amount > 0) {
+                    return $selected->sum_amount = $sum_amount - ($sum_amount * $selected->show_minus) / 100;
+                } else {
+                    return $selected->sum_amount = $sum_amount;
+                }
+            });
+
+            $urgent = Models\Program::select('program.*', 'organization.name', 'organization.status')->join('organization', 'program.organization_id', 'organization.id')->where('is_publish', 1)->where('is_urgent', 1)->whereNotNull('program.approved_at')->where('end_date', '>', date('Y-m-d'))->orderBy('program.created_at', 'DESC')->limit(6)->get();
+            $urgent->map(function ($urgent, $key) {
+                $sum_amount = Models\Transaction::where('program_id', $urgent->id)->where('status', 'success')->sum('nominal_final');
+                if ($urgent->show_minus > 0 && !is_null($urgent->show_minus) && $sum_amount > 0) {
+                    return $urgent->sum_amount = $sum_amount - ($sum_amount * $urgent->show_minus) / 100;
+                } else {
+                    return $urgent->sum_amount = $sum_amount;
+                }
+            });
+
+            $newest = Models\Program::where('is_publish', 1)->select('program.*', 'organization.name', 'organization.status')->join('organization', 'program.organization_id', 'organization.id')->where('is_show_home', 1)->where('is_recommended', 0)->whereNotNull('program.approved_at')->where('end_date', '>', date('Y-m-d'))->orderBy('program.created_at', 'DESC')->limit(7)->get();
+            $newest->map(function ($newest, $key) {
+                $sum_amount = Models\Transaction::where('program_id', $newest->id)->where('status', 'success')->sum('nominal_final');
+                if ($newest->show_minus > 0 && !is_null($newest->show_minus) && $sum_amount > 0) {
+                    return $newest->sum_amount = $sum_amount - ($sum_amount * $newest->show_minus) / 100;
+                } else {
+                    return $newest->sum_amount = $sum_amount;
+                }
+            });
+            return compact('category', 'selected', 'newest', 'urgent');
         });
 
-        $newest = Models\Program::where('is_publish', 1)->select('program.*', 'organization.name', 'organization.status')->join('organization', 'program.organization_id', 'organization.id')->where('is_show_home', 1)->where('is_recommended', 0)->whereNotNull('program.approved_at')->where('end_date', '>', date('Y-m-d'))->orderBy('program.created_at', 'DESC')->limit(7)->get();
-        $newest->map(function ($newest, $key) {
-            $sum_amount = Models\Transaction::where('program_id', $newest->id)->where('status', 'success')->sum('nominal_final');
-            if ($newest->show_minus > 0 && !is_null($newest->show_minus) && $sum_amount > 0) {
-                return $newest->sum_amount = $sum_amount - ($sum_amount * $newest->show_minus) / 100;
-            } else {
-                return $newest->sum_amount = $sum_amount;
-            }
-        });
-        return view('public.index', compact('category', 'slider', 'popup', 'selected', 'newest', 'urgent'));
+        $data = array_merge($banners, $programs);
+
+        return view('public.index', $data);
     }
 
     public function aboutUs()
